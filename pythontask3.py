44d1f2d28275c0452106ff4b746e38cc9e24726f
Nosakhare Edokpayi <co.nosakhare@gmail.com>
2020-04-24 10:40:43 +0100

    Update pythontask3.py

--- pythontask3.py
+++ pythontask3.py
@@ -1,139 +1,89 @@
 from random import randint
 
+levels = {
+    "1": {
+        "name": "Easy",
+        "start_message": ""f"What's the secret number to the treasure Chest",
+        "min_num": 1,
+        "max_num": 6,
+        "guesses": 6
+    },
+    "2": {
+        "name": "Medium",
+        "start_message": "Guess the secret number to open the safe",
+        "min_num": 1,
+        "max_num": 20,
+        "guesses": 4
+    },
+    "3": {
+        "name": "Hard",
+        "start_message": "Guess the secret number to the unlock the vault",
+        "min_num": 1,
+        "max_num": 50,
+        "guesses": 3
+    }
+}
 
-def level_three():
-    guess_count = 3
-    number = randint(1, 50)
+
+def play_game():
+    while True:
+        play = input("Yes/No: ")
+        if play.lower() == "yes":
+            start()
+        elif play.lower() == "no":
+            print("Okay, maybe next time!")
+            exit(0)
+        else:
+            print("Wrong input!")
+
+
+def game(level):
+    game_plot = levels[level]
+    number = randint(game_plot["min_num"], game_plot["max_num"])
     print(number)
-    print(f"Guess the secret number to the unlock the vault\nYou have {guess_count} guesses")
+    print(f"{game_plot['start_message']}\nYou have {game_plot['guesses']} number of guess(es) left.")
+
     while True:
         try:
-            answer = input("hint: number is between 1 and 50?\nAnswer: ")
-            guess_count -= 1
-            if int(answer) == number:
-                print("You win the game!\nRestart Game?")
-                while True:
-                    restart_game = input("Yes/No: ")
-                    if restart_game.lower() == "yes":
-                        start()
-                    elif restart_game.lower() == "no":
-                        print("Okay, maybe next time!")
-                        exit(0)
-                    else:
-                        print("Wrong input!")
-            elif guess_count == 0:
-                print("You have no more guesses left.\nYou lose!")
-                print("Do you want to play again")
-                while True:
-                    play_again = input("Yes/No: ")
-                    if play_again.lower() == "yes":
-                        start()
-                    elif play_again.lower() == "no":
-                        print("Okay, maybe next time!")
-                        exit(0)
-                    else:
-                        print("Wrong input!")
-            else:
-                print(f"Wrong try again! you have {guess_count} guess(es) left")
-        except ValueError:
-            print(f"Please enter a number!\nYou have {guess_count} guess(es) left")
+            answer = input(f"hint: number is between {game_plot['min_num']} and {game_plot['max_num']}?\nAnswer: ")
+            game_plot["guesses"] -= 1
 
-
-def level_two():
-    guess_count = 4
-    number = randint(1, 20)
-    print(number)
-    print(f"Guess the secret number to open the safe\nYou have {guess_count} guesses")
-    while True:
-        try:
-            answer = input("hint: number is between 1 and 20?\nAnswer: ")
-            guess_count -= 1
             if int(answer) == number:
+                if level == "3":
+                    print("You win the game!\nRestart Game?")
+                    play_game()
                 print("You got it right!\nEnter next level?")
                 while True:
                     next_level = input("Yes/No: ")
                     if next_level.lower() == "yes":
-                        level_three()
+                        game(str(int(level)+1))
                     elif next_level.lower() == "no":
                         print("Okay, maybe next time!")
                         exit(0)
                     else:
                         print("Wrong input!")
-            elif guess_count == 0:
+            elif game_plot["guesses"] == 0:
                 print("You have no more guesses left.\nYou lose!")
                 print("Do you want to play again")
-                while True:
-                    play_again = input("Yes/No: ")
-                    if play_again.lower() == "yes":
-                        start()
-                    elif play_again.lower() == "no":
-                        print("Okay, maybe next time!")
-                        exit(0)
-                    else:
-                        print("Wrong input!")
+                play_game()
             else:
-                print(f"Wrong try again! you have {guess_count} guess(es) left")
+                print(f"Wrong try again!\nYou have {game_plot['guesses']} guess(es) left")
         except ValueError:
-            print(f"Please enter a number!\nYou have {guess_count} guess(es) left")
-
-
-def level_one():
-    guess_count = 6
-    number = randint(1, 10)
-    print(number)
-    print(f"What's the secret number to the treasure Chest\nYou have {guess_count} guesses")
-    while True:
-        try:
-            answer = input("hint: number is between 1 and 10?\nAnswer: ")
-            guess_count -= 1
-            if int(answer) == number:
-                print("You got it right!\nEnter next level?")
-                while True:
-                    next_level = input("Yes/No: ")
-                    if next_level.lower() == "yes":
-                        level_two()
-                    elif next_level.lower() == "no":
-                        print("Okay, maybe next time!")
-                        exit(0)
-            elif guess_count == 0:
-                print("You have no more guesses left.\nYou lose!")
-                print("Do you want to play again")
-                while True:
-                    play_again = input("Yes/No: ")
-                    if play_again.lower() == "yes":
-                        pass
-                    elif play_again.lower() == "no":
-                        print("Okay, maybe next time!")
-                        exit(0)
-                    else:
-                        print("Wrong input!")
-            else:
-                print(f"Wrong try again! you have {guess_count} guess(es) left")
-        except ValueError:
-            print(f"Please enter a number!\nYou have {guess_count} guess(es) left")
+            print(f"Please enter a number!\nYou have {game_plot['guesses']} guess(es) left")
 
 
 def start():
     print("Select your difficulty level\nEasy: 1\nMedium: 2\nHard: 3")
     while True:
-        level = input("Enter level number: ")
-        if level == "1":
-            level_one()
-        elif level == "2":
-            level_two()
-        elif level == "3":
-            level_three()
-        else:
-            print("Wrong input!")
+        try:
+            level = input("Enter level number: ")
+            if level == "1" or level == "2" or level == "3":
+                game(level)
+            else:
+                print("Wrong input!")
+        except ValueError:
+            print(f"Please enter a number!")
 
 
 print("Hi gamer, Welcome to Number Guess!\nAre you ready to play?")
-while True:
-    ready = input("Yes/No: ")
-    if ready.lower() == "yes":
-        start()
-    elif ready.lower() == "no":
-        print("Okay, maybe next time!")
-        exit(0)
-    else:
-        print("Wrong input!")
+play_game()
